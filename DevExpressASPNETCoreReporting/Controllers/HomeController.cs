b84b93118d87e601ca7e27ba1e41ee626bc28602
Lucas Williams <lucas@lucas-williams.com>
2017-11-12 23:09:39 +1100

    Report Viewer work
    Not quite there as DevExpress extensions aren't working in .NET Core

--- DevExpressASPNETCoreReporting/Controllers/HomeController.cs
+++ DevExpressASPNETCoreReporting/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 using DevExpressASPNETCoreReporting.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.IO;
+using System.Text;
 
 namespace DevExpressASPNETCoreReporting.Controllers {
     public class HomeController : Controller {
@@ -32,15 +34,27 @@ namespace DevExpressASPNETCoreReporting.Controllers {
             return View(model);
         }
 
-        public IActionResult ReportViewer() {
-            var clientSideModelGenerator = new WebDocumentViewerClientSideModelGenerator();
-            var clientSideModelSettings = new ClientSideModelSettings { IncludeLocalization = false };
-            //var report = new CategoriesReport();
-            var modelString = clientSideModelGenerator.GetJsonModelScript((XtraReport) null/*report*/, "/DXXRDV", clientSideModelSettings);
-            var model = new ClientControlModel {
-                ModelJson = modelString
-            };
-            return View(model);
+        public IActionResult ReportViewer(int id) {
+            var optionsBuilder = new DbContextOptionsBuilder<ReportContext>();
+            optionsBuilder.UseSqlServer("Data Source=LOCALHOST\\SQLExpress;Initial Catalog=ManageWithReports;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            ReportContext _db = new ReportContext(optionsBuilder.Options);
+
+            XtraReport myReport = new XtraReport();
+            Report dbReport = _db.Reports.FirstOrDefault(r => r.Id == id);
+            MemoryStream ms = new MemoryStream(dbReport.Content);
+            myReport.LoadLayoutFromXml(ms);
+
+            //var clientSideModelGenerator = new WebDocumentViewerClientSideModelGenerator();
+            //var clientSideModelSettings = new ClientSideModelSettings { IncludeLocalization = false };
+            ////var report = new CategoriesReport();
+            //var modelString = clientSideModelGenerator.GetJsonModelScript((XtraReport) null/*report*/, "/DXXRDV", clientSideModelSettings);
+            //var model = new ClientControlModel {
+            //    ModelJson = modelString
+            //};
+
+
+            return View(myReport);
+
         }
 
         public IActionResult Error() {
